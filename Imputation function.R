5be5045ddaa652f57713e62a78dca1dc87dc02b5
Fleur Boogaard <fleurboogaard@hotmail.com>
2020-02-23 16:52:34 +0100

    Updated imputation function
    I did not want to push the full updated Skeleton because I thought I may have changed something accidentally

---
+++ Imputation function.R
@@ -0,0 +1,23 @@
+imputations <- function(K, M, completedata, weather) { # I used the standard setup where k = 5 and m = 5
+  # completedata <- fulldata(weather, ovitrap_cleaned, ovitrap_original)
+  # completedata <- full_data_NA
+  
+  ######### Imputing ovitrap data #########
+  
+  ovitrap_imputed <- kNN(completedata, variable = "value", k=K, imp_var = F)
+  
+  ######### Imputing weather data #########
+  weather_missing <- weather
+  
+  imputation_mice <- mice(weather_missing, m = M, print = FALSE) # Impute by Multiple Imputations with Chained Equations
+  imputed_weather <- complete(imputation_mice)
+  
+  names(imputed_weather)[names(imputed_weather) == "adm_level"] <- "adm" 
+  
+  result <- merge(ovitrap_imputed[,1:3], imputed_weather, by = c("adm", "date"))
+  result$value <- as.numeric(as.character(result$value))
+  
+  output <- list(result, imputed_weather)
+  
+  return(result)
+}
