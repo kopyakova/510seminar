a09a251e8e597aa1c9eb8642624a3e529f5b6946
Anna Kopyakova <55054346+kopyakova@users.noreply.github.com>
2020-03-14 18:14:24 +0100

    Update XGBoost.R

--- Source/XGBoost.R
+++ Source/XGBoost.R
@@ -1,21 +1,20 @@
 library(xgboost)
 
 set.seed(510)
-df_test <- read.csv('/Users/Yur/Downloads/testing_imputed.csv')
-df_train <- read.csv('/Users/Yur/Downloads/training_imputed.csv')
+df_test  <- read.csv('../Data/test_data.csv')
+df_train <- read.csv('../Data/training_data.csv')
 
 # remove unused variables
-df_test['longitude']       <- NULL
-df_test['latitude']        <- NULL
-df_test['value_indicator'] <- NULL
+df_train <- df_train[,!(names(df_train) == "X")]
+df_train <- df_train[,!(names(df_train) == "adm")]
+df_train <- df_train[,!(names(df_train) == "date")]
+df_train <- df_train[ ,!(names(df_train) == "value_indicator")]
 
-df_train['longitude']       <- NULL
-df_train['latitude']        <- NULL
-df_train['value_indicator'] <- NULL
+df_test <- df_test[,!(names(df_test) == "X")]
+df_test <- df_test[,!(names(df_test) == "adm")]
+df_test <- df_test[,!(names(df_test) == "date")]
+df_test <- df_test[ ,!(names(df_test) == "value_indicator")]
 
-# remove index, adm and date
-df_test  <- df_test[,4:ncol(df_test)]
-df_train <- df_train[,4:ncol(df_train)]
 
 #---------------------------------------------------------#
 # k-Fold cross validation over eta, nrounds and max depth #
@@ -65,5 +64,25 @@ bst <- xgboost(data = as.matrix(df_train[,!names(data_train)%in%c('value')]),
                objective = "reg:squarederror", 
                verbose=1)
 
-pred <- predict(bst, as.matrix(df_test[,!names(data_test)%in%c('value')]), outputmargin=TRUE)
-rmse(as.numeric(df_test[,'value']), as.numeric(pred))
+######### XGBoost evaluation ######### 
+source("../Results/Auxiliary_functions_results.R", echo = F)
+true_train <- as.numeric(df_train[,'value'])
+true_test  <- as.numeric(df_test[,'value'])
+
+# Get predictions
+xg_pred_train <- predict(bst, as.matrix(df_train[,!names(data_train)%in%c('value')]), outputmargin=TRUE)
+xg_pred_test  <- predict(bst, as.matrix(df_test[,!names(data_test)%in%c('value')]), outputmargin=TRUE)
+
+# Evaluate results
+xg_results <- evaluate_results(true_train = true_train, pred_train = xg_pred_train, 
+                                    true_test  = true_test, pred_test = xg_pred_test, 
+                                    method_name = "XGBoost")  
+
+# Evaluate residuls
+xg_resid   <- evaluate_residuals(predicted_test = xg_pred_test, true_test = true_test, 
+                                    method = "XGBoost")
+
+#Print plots with the results
+print(xg_results$plot)
+xg_resid$resid_vs_pred
+xg_resid$resid_vs_actual
